954fc23dcd088cd1c95298e0068aa211d2bb42a7
David-Lochan <davidlochanboro@gmail.com>
2025-12-24 22:42:49 +0530

    Implemented async/await with multiple tasks using a real-life canteen serving example

---
+++ AdvancedCSharp/AsyncAwait/MultipleTasks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("🏫 College canteen opened...\n");
+
+        List<Task> orders = new List<Task>
+        {
+            ServeStudentAsync("Arnold", "Thali", 3000),
+            ServeStudentAsync("Pintu", "Biryani", 2000),
+            ServeStudentAsync("Trisha", "Samosa & Tea", 1000),
+            ServeStudentAsync("David", "Coffee", 1500)
+        };
+
+        await Task.WhenAll(orders);
+
+        Console.WriteLine("\nAll students served. Canteen closed.");
+    }
+
+    static async Task ServeStudentAsync(string name, string food, int time)
+    {
+        Console.WriteLine($"Order received from {name} for {food}");
+        await Task.Delay(time);
+        Console.WriteLine($"{food} served to {name}");
+    }
+}
