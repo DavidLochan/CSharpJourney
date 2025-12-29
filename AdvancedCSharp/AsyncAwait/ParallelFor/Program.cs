ed12cbd626c8137739c7e124f7fca2d986b77070
David-Lochan <davidlochanboro@gmail.com>
2025-12-29 19:00:58 +0530

    Add Parallel.For example and SemaphoreSlim example

---
+++ AdvancedCSharp/AsyncAwait/ParallelFor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Washing plates using Parallel.For\n");
+
+        Parallel.For(1, 6, plateNumber =>
+        {
+            Console.WriteLine($"Plate {plateNumber} washed by thread {Environment.CurrentManagedThreadId}");
+        });
+
+        Console.WriteLine("\nAll plates washed");
+    }
+}
---
+++ AdvancedCSharp/AsyncAwait/SemaphoreSlim/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static SemaphoreSlim toilet = new SemaphoreSlim(2); // only 2 allowed
+
+    static async Task Main()
+    {
+        Console.WriteLine("Toilet opened\n");
+
+        Task[] people =
+        {
+            UseToilet("David"),
+            UseToilet("Arnold"),
+            UseToilet("Pintu"),
+            UseToilet("Naresh"),
+            UseToilet("Borad")
+        };
+
+        await Task.WhenAll(people);
+
+        Console.WriteLine("\nEveryone is done");
+    }
+
+    static async Task UseToilet(string name)
+    {
+        await toilet.WaitAsync(); // wait for permission
+
+        Console.WriteLine($"{name} entered toilet");
+        await Task.Delay(2000);   // using toilet
+        Console.WriteLine($"{name} exited toilet");
+
+        toilet.Release(); // allow next person
+    }
+}
