3fa2262d2413f4ab46937be60a32bb798fddc13f
David-Lochan <davidlochanboro@gmail.com>
2026-01-06 23:59:58 +0530

    button click

--- Maui/AlohaWorld/MainPage.xaml.cs
+++ Maui/AlohaWorld/MainPage.xaml.cs
@@ -2,22 +2,16 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object? sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        count++;
+        MessageLabel.Text = $"Button clicked {count} times";
+    }
 }
