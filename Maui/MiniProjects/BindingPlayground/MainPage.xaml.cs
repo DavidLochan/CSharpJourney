0f222d6e56e27b93665f2b976effd375eb4c1f5e
David-Lochan <davidlochanboro@gmail.com>
2026-01-07 18:43:47 +0530

    BindingPlayground demonstrating view-to-view data binding (Chapter 3)

---
+++ Maui/MiniProjects/BindingPlayground/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BindingPlayground;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Maui/MiniProjects/BindingPlayground/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BindingPlayground;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
