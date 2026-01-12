379b92629b39ed8248885bc5d6473ff47147d92c
David-Lochan <davidlochanboro@gmail.com>
2026-01-12 12:53:02 +0530

    fixed nullable warning using required , added static movie data source, display static movies using BindableLayout

--- Maui/MiniProjects/MauiMovies/MainPage.xaml.cs
+++ Maui/MiniProjects/MauiMovies/MainPage.xaml.cs
@@ -4,9 +4,20 @@ using System.Collections.ObjectModel;
 namespace MauiMovies;
 
 public partial class MainPage : ContentPage
-{
+{   
+	 public ObservableCollection<Movie> Movies { get; set; }
+
     public MainPage()
     {
         InitializeComponent();
+		
+		Movies = new ObservableCollection<Movie>
+        {
+            new Movie { Title="Inception", Rating=8.8, Poster="movie1.jpg" },
+            new Movie { Title="Interstellar", Rating=8.6, Poster="movie2.jpg" },
+            new Movie { Title="Dune", Rating=8.2, Poster="movie3.jpg" }
+        };
+
+        BindingContext = this;
     }
 }
--- Maui/MiniProjects/MauiMovies/Models/Movie.cs
+++ Maui/MiniProjects/MauiMovies/Models/Movie.cs
@@ -2,7 +2,7 @@ namespace MauiMovies.Models;
 
 public class Movie
 {
-    public string Title { get; set; }
-    public string Poster { get; set; }
-    public double Rating { get; set; }
+    public required string Title { get; set; }
+    public required string Poster { get; set; } 
+    public required double Rating { get; set; } 
 }
