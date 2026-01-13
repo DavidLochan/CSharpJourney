64691e6b0ab40eb9dcdabd0d94696f0730ad628c
David-Lochan <davidlochanboro@gmail.com>
2026-01-13 19:55:28 +0530

    Added Functional Genre Selection above movie list

--- Maui/MiniProjects/MauiMovies/MainPage.xaml.cs
+++ Maui/MiniProjects/MauiMovies/MainPage.xaml.cs
@@ -6,6 +6,7 @@ namespace MauiMovies;
 public partial class MainPage : ContentPage
 {
     public ObservableCollection<Movie> Movies { get; set; }
+	private List<Movie> _allMovies;
 
 	         private Movie? _selectedMovie;
     public Movie? SelectedMovie
@@ -36,41 +37,42 @@ public partial class MainPage : ContentPage
 
         Movies = new ObservableCollection<Movie>
         {
-        new Movie { Title="Inception", Rating=8.8, Poster="movie1.jpg" },
-        new Movie { Title="Interstellar", Rating=8.6, Poster="movie2.jpg" },
-        new Movie { Title="Dune", Rating=8.2, Poster="movie3.jpg" },
-		new Movie { Title="The Dark Knight", Rating=9.0, Poster="movie4.jpg" },
-        new Movie { Title="Blade Runner 2049", Rating=8.1, Poster="movie5.jpg" },
-        new Movie { Title="Tenet", Rating=7.8, Poster="movie6.jpg" },
-        new Movie { Title="Arrival", Rating=7.9, Poster="movie7.jpg" },
-        new Movie { Title="The Matrix", Rating=8.7, Poster="movie8.jpg" },
-        new Movie { Title="Avatar", Rating=7.8, Poster="movie9.jpg" },
-        new Movie { Title="Gravity", Rating=7.7, Poster="movie10.jpg" },
+        new Movie { Title="Inception", Rating=8.8, Poster="movie1.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Interstellar", Rating=8.6, Poster="movie2.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Dune", Rating=8.2, Poster="movie3.jpg", Genre="Sci-Fi" },
+		new Movie { Title="The Dark Knight", Rating=9.0, Poster="movie4.jpg", Genre="Action" },
+        new Movie { Title="Blade Runner 2049", Rating=8.1, Poster="movie5.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Tenet", Rating=7.8, Poster="movie6.jpg", Genre="Action" },
+        new Movie { Title="Arrival", Rating=7.9, Poster="movie7.jpg", Genre="Sci-Fi" },
+        new Movie { Title="The Matrix", Rating=8.7, Poster="movie8.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Avatar", Rating=7.8, Poster="movie9.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Gravity", Rating=7.7, Poster="movie10.jpg", Genre="Sci-Fi" },
 
-        new Movie { Title="Oppenheimer", Rating=8.9, Poster="movie11.jpg" },
-        new Movie { Title="The Prestige", Rating=8.5, Poster="movie12.jpg" },
-        new Movie { Title="Ex Machina", Rating=7.7, Poster="movie13.jpg" },
-        new Movie { Title="Mad Max Fury Road", Rating=8.1, Poster="movie14.jpg" },
-        new Movie { Title="Her", Rating=8.0, Poster="movie15.jpg" },
-        new Movie { Title="District 9", Rating=7.9, Poster="movie16.jpg" },
-        new Movie { Title="Moon", Rating=7.8, Poster="movie17.jpg" },
-        new Movie { Title="Annihilation", Rating=6.9, Poster="movie18.jpg" },
-        new Movie { Title="Edge of Tomorrow", Rating=7.9, Poster="movie19.jpg" },
-        new Movie { Title="Minority Report", Rating=7.6, Poster="movie20.jpg" },
+        new Movie { Title="Oppenheimer", Rating=8.9, Poster="movie11.jpg", Genre="Drama" },
+        new Movie { Title="The Prestige", Rating=8.5, Poster="movie12.jpg", Genre="Drama" },
+        new Movie { Title="Ex Machina", Rating=7.7, Poster="movie13.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Mad Max Fury Road", Rating=8.1, Poster="movie14.jpg", Genre="Action" },
+        new Movie { Title="Her", Rating=8.0, Poster="movie15.jpg", Genre="Drama" },
+        new Movie { Title="District 9", Rating=7.9, Poster="movie16.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Moon", Rating=7.8, Poster="movie17.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Annihilation", Rating=6.9, Poster="movie18.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Edge of Tomorrow", Rating=7.9, Poster="movie19.jpg", Genre="Action" },
+        new Movie { Title="Minority Report", Rating=7.6, Poster="movie20.jpg", Genre="Sci-Fi" },
 
-        new Movie { Title="Alien", Rating=8.5, Poster="movie21.jpg" },
-        new Movie { Title="Aliens", Rating=8.4, Poster="movie22.jpg" },
-        new Movie { Title="Prometheus", Rating=7.0, Poster="movie23.jpg" },
-        new Movie { Title="The Martian", Rating=8.0, Poster="movie24.jpg" },
-        new Movie { Title="Elysium", Rating=6.6, Poster="movie25.jpg" },
-        new Movie { Title="Oblivion", Rating=7.0, Poster="movie26.jpg" },
-        new Movie { Title="Looper", Rating=7.4, Poster="movie27.jpg" },
-        new Movie { Title="Sunshine", Rating=7.2, Poster="movie28.jpg" },
-        new Movie { Title="Ready Player One", Rating=7.5, Poster="movie29.jpg" },
-        new Movie { Title="Tron Legacy", Rating=6.8, Poster="movie30.jpg" }
+        new Movie { Title="Alien", Rating=8.5, Poster="movie21.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Aliens", Rating=8.4, Poster="movie22.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Prometheus", Rating=7.0, Poster="movie23.jpg", Genre="Sci-Fi" },
+        new Movie { Title="The Martian", Rating=8.0, Poster="movie24.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Elysium", Rating=6.6, Poster="movie25.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Oblivion", Rating=7.0, Poster="movie26.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Looper", Rating=7.4, Poster="movie27.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Sunshine", Rating=7.2, Poster="movie28.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Ready Player One", Rating=7.5, Poster="movie29.jpg", Genre="Sci-Fi" },
+        new Movie { Title="Tron Legacy", Rating=6.8, Poster="movie30.jpg", Genre="Sci-Fi" }
         };
 
         BindingContext = this;
+		_allMovies = Movies.ToList();
     }
 	private async void OnPointerEntered(object sender, PointerEventArgs e)
     {
@@ -110,5 +112,35 @@ public partial class MainPage : ContentPage
     {
         SelectedMovie = null;
     }
+	  public ObservableCollection<string> Genres { get; } =
+    new() { "All", "Sci-Fi", "Action", "Drama" };
+
+private string _selectedGenre = "All";
+public string SelectedGenre
+{
+    get => _selectedGenre;
+    set
+    {
+        _selectedGenre = value;
+        OnPropertyChanged();
+        ApplyGenreFilter();
+    }
+}
+private void ApplyGenreFilter()
+{
+    Movies.Clear();
+
+    var filtered = SelectedGenre == "All"
+        ? _allMovies
+        : _allMovies.Where(m => m.Genre == SelectedGenre);
+
+    foreach (var movie in filtered)
+        Movies.Add(movie);
+}
+private void OnGenreClicked(object sender, EventArgs e)
+{
+    if (sender is Button btn)
+        SelectedGenre = btn.Text;
+}
 
 }
--- Maui/MiniProjects/MauiMovies/Models/Movie.cs
+++ Maui/MiniProjects/MauiMovies/Models/Movie.cs
@@ -5,4 +5,5 @@ public class Movie
     public string Title { get; set; } = "";
     public double Rating { get; set; }
     public string? Poster { get; set; }
+    public string Genre { get; set; }
 }
