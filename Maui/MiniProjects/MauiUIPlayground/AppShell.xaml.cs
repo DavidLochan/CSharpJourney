d4eda19d8905a03268e33466e76cf8f09c66be58
David-Lochan <davidlochanboro@gmail.com>
2026-02-05 23:11:10 +0530

    fresh MAUI UI playground

---
+++ Maui/MiniProjects/MauiUIPlayground/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiUIPlayground;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Maui/MiniProjects/MauiUIPlayground/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiUIPlayground;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
