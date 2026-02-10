798149058fc78ca099b8afc884b71d4f3e4dd4f7
David-Lochan <davidlochanboro@gmail.com>
2026-02-10 22:21:57 +0530

    name internal controls in MildredStepper, wire button click handlers in MildredStepper, implemented internal value logic for MildredStepper

--- Maui/MiniProjects/MauiControlsPractice/Controls/MildredStepper.xaml.cs
+++ Maui/MiniProjects/MauiControlsPractice/Controls/MildredStepper.xaml.cs
@@ -2,8 +2,24 @@ namespace MauiControlsPractice.Controls;
 
 public partial class MildredStepper : ContentView
 {
+    private int _value;
+
     public MildredStepper()
     {
         InitializeComponent();
+        _value = 0;
+        ValueEntry.Text = _value.ToString();
+    }
+
+    private void MinusButton_Clicked(object sender, EventArgs e)
+    {
+        _value--;
+        ValueEntry.Text = _value.ToString();
+    }
+
+    private void PlusButton_Clicked(object sender, EventArgs e)
+    {
+        _value++;
+        ValueEntry.Text = _value.ToString();
     }
 }
