7577b15b26e993f7b0282a083a7d673319bd9376
David-Lochan <davidlochanboro@gmail.com>
2026-02-20 19:49:54 +0530

    MacDeskLab

---
+++ Maui/MiniProjects/MacDeskLab/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MacDeskLab;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+	protected override Window CreateWindow(IActivationState? activationState)
+{
+    return new Window(new AppShell())
+    {
+        Title = "MacDeskLab",
+        MinimumWidth = 1000,
+        MinimumHeight = 650
+    };
+}
+}
